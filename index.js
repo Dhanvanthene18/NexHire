f8057964c96ecca2506d27552bc3d7431710404b
S.Dhanvanthene <dhanasaravanan150603@gmail.com>
2026-07-30 11:44:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,217 +1,155 @@
-'use strict';
+self.Flatted = (function (exports) {
+  'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
+  function _typeof(o) {
+    "@babel/helpers - typeof";
 
-var picocolors = require('picocolors');
-var jsTokens = require('js-tokens');
-var helperValidatorIdentifier = require('@babel/helper-validator-identifier');
+    return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) {
+      return typeof o;
+    } : function (o) {
+      return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o;
+    }, _typeof(o);
+  }
 
-function isColorSupported() {
-  return (typeof process === "object" && (process.env.FORCE_COLOR === "0" || process.env.FORCE_COLOR === "false") ? false : picocolors.isColorSupported
-  );
-}
-const compose = (f, g) => v => f(g(v));
-function buildDefs(colors) {
-  return {
-    keyword: colors.cyan,
-    capitalized: colors.yellow,
-    jsxIdentifier: colors.yellow,
-    punctuator: colors.yellow,
-    number: colors.magenta,
-    string: colors.green,
-    regex: colors.magenta,
-    comment: colors.gray,
-    invalid: compose(compose(colors.white, colors.bgRed), colors.bold),
-    gutter: colors.gray,
-    marker: compose(colors.red, colors.bold),
-    message: compose(colors.red, colors.bold),
-    reset: colors.reset
+  /// <reference types="../types/index.d.ts" />
+
+  // (c) 2020-present Andrea Giammarchi
+
+  var $parse = JSON.parse,
+    $stringify = JSON.stringify;
+  var keys = Object.keys;
+  var Primitive = String; // it could be Number
+  var primitive = 'string'; // it could be 'number'
+
+  var ignore = {};
+  var object = 'object';
+  var noop = function noop(_, value) {
+    return value;
   };
-}
-const defsOn = buildDefs(picocolors.createColors(true));
-const defsOff = buildDefs(picocolors.createColors(false));
-function getDefs(enabled) {
-  return enabled ? defsOn : defsOff;
-}
-
-const sometimesKeywords = new Set(["as", "async", "from", "get", "of", "set"]);
-const NEWLINE$1 = /\r\n|[\n\r\u2028\u2029]/;
-const BRACKET = /^[()[\]{}]$/;
-let tokenize;
-const JSX_TAG = /^[a-z][\w-]*$/i;
-const getTokenType = function (token, offset, text) {
-  if (token.type === "name") {
-    const tokenValue = token.value;
-    if (helperValidatorIdentifier.isKeyword(tokenValue) || helperValidatorIdentifier.isStrictReservedWord(tokenValue, true) || sometimesKeywords.has(tokenValue)) {
-      return "keyword";
-    }
-    if (JSX_TAG.test(tokenValue) && (text[offset - 1] === "<" || text.slice(offset - 2, offset) === "</")) {
-      return "jsxIdentifier";
-    }
-    const firstChar = String.fromCodePoint(tokenValue.codePointAt(0));
-    if (firstChar !== firstChar.toLowerCase()) {
-      return "capitalized";
-    }
-  }
-  if (token.type === "punctuator" && BRACKET.test(token.value)) {
-    return "bracket";
-  }
-  if (token.type === "invalid" && (token.value === "@" || token.value === "#")) {
-    return "punctuator";
-  }
-  return token.type;
-};
-tokenize = function* (text) {
-  let match;
-  while (match = jsTokens.default.exec(text)) {
-    const token = jsTokens.matchToToken(match);
-    yield {
-      type: getTokenType(token, match.index, text),
-      value: token.value
+  var primitives = function primitives(value) {
+    return value instanceof Primitive ? Primitive(value) : value;
+  };
+  var Primitives = function Primitives(_, value) {
+    return _typeof(value) === primitive ? new Primitive(value) : value;
+  };
+  var resolver = function resolver(input, lazy, parsed, $) {
+    return function (output) {
+      for (var ke = keys(output), length = ke.length, y = 0; y < length; y++) {
+        var k = ke[y];
+        var value = output[k];
+        if (value instanceof Primitive) {
+          var tmp = input[+value];
+          if (_typeof(tmp) === object && !parsed.has(tmp)) {
+            parsed.add(tmp);
+            output[k] = ignore;
+            lazy.push({
+              o: output,
+              k: k,
+              r: tmp
+            });
+          } else output[k] = $.call(output, k, tmp);
+        } else if (output[k] !== ignore) output[k] = $.call(output, k, value);
+      }
+      return output;
     };
-  }
-};
-function highlight(text) {
-  if (text === "") return "";
-  const defs = getDefs(true);
-  let highlighted = "";
-  for (const {
-    type,
-    value
-  } of tokenize(text)) {
-    if (type in defs) {
-      highlighted += value.split(NEWLINE$1).map(str => defs[type](str)).join("\n");
-    } else {
-      highlighted += value;
-    }
-  }
-  return highlighted;
-}
-
-let deprecationWarningShown = false;
-const NEWLINE = /\r\n|[\n\r\u2028\u2029]/;
-function getMarkerLines(loc, source, opts, startLineBaseZero) {
-  const startLoc = Object.assign({
-    column: 0,
-    line: -1
-  }, loc.start);
-  const endLoc = Object.assign({}, startLoc, loc.end);
-  const {
-    linesAbove = 2,
-    linesBelow = 3
-  } = opts || {};
-  const startLine = startLoc.line - startLineBaseZero;
-  const startColumn = startLoc.column;
-  const endLine = endLoc.line - startLineBaseZero;
-  const endColumn = endLoc.column;
-  let start = Math.max(startLine - (linesAbove + 1), 0);
-  let end = Math.min(source.length, endLine + linesBelow);
-  if (startLine === -1) {
-    start = 0;
-  }
-  if (endLine === -1) {
-    end = source.length;
-  }
-  const lineDiff = endLine - startLine;
-  const markerLines = {};
-  if (lineDiff) {
-    for (let i = 0; i <= lineDiff; i++) {
-      const lineNumber = i + startLine;
-      if (!startColumn) {
-        markerLines[lineNumber] = true;
-      } else if (i === 0) {
-        const sourceLength = source[lineNumber - 1].length;
-        markerLines[lineNumber] = [startColumn, sourceLength - startColumn + 1];
-      } else if (i === lineDiff) {
-        markerLines[lineNumber] = [0, endColumn];
-      } else {
-        const sourceLength = source[lineNumber - i].length;
-        markerLines[lineNumber] = [0, sourceLength];
-      }
-    }
-  } else {
-    if (startColumn === endColumn) {
-      if (startColumn) {
-        markerLines[startLine] = [startColumn, 0];
-      } else {
-        markerLines[startLine] = true;
-      }
-    } else {
-      markerLines[startLine] = [startColumn, endColumn - startColumn];
-    }
-  }
-  return {
-    start,
-    end,
-    markerLines
   };
-}
-function codeFrameColumns(rawLines, loc, opts = {}) {
-  const shouldHighlight = opts.forceColor || isColorSupported() && opts.highlightCode;
-  const startLineBaseZero = (opts.startLine || 1) - 1;
-  const defs = getDefs(shouldHighlight);
-  const lines = rawLines.split(NEWLINE);
-  const {
-    start,
-    end,
-    markerLines
-  } = getMarkerLines(loc, lines, opts, startLineBaseZero);
-  const hasColumns = loc.start && typeof loc.start.column === "number";
-  const numberMaxWidth = String(end + startLineBaseZero).length;
-  const highlightedLines = shouldHighlight ? highlight(rawLines) : rawLines;
-  let frame = highlightedLines.split(NEWLINE, end).slice(start, end).map((line, index) => {
-    const number = start + 1 + index;
-    const paddedNumber = ` ${number + startLineBaseZero}`.slice(-numberMaxWidth);
-    const gutter = ` ${paddedNumber} |`;
-    const hasMarker = markerLines[number];
-    const lastMarkerLine = !markerLines[number + 1];
-    if (hasMarker) {
-      let markerLine = "";
-      if (Array.isArray(hasMarker)) {
-        const markerSpacing = line.slice(0, Math.max(hasMarker[0] - 1, 0)).replace(/[^\t]/g, " ");
-        const numberOfMarkers = hasMarker[1] || 1;
-        markerLine = ["\n ", defs.gutter(gutter.replace(/\d/g, " ")), " ", markerSpacing, defs.marker("^").repeat(numberOfMarkers)].join("");
-        if (lastMarkerLine && opts.message) {
-          markerLine += " " + defs.message(opts.message);
-        }
+  var set = function set(known, input, value) {
+    var index = Primitive(input.push(value) - 1);
+    known.set(value, index);
+    return index;
+  };
+
+  /**
+   * Converts a specialized flatted string into a JS value.
+   * @param {string} text
+   * @param {(this: any, key: string, value: any) => any} [reviver]
+   * @returns {any}
+   */
+  var parse = function parse(text, reviver) {
+    var input = $parse(text, Primitives).map(primitives);
+    var $ = reviver || noop;
+    var value = input[0];
+    if (_typeof(value) === object && value) {
+      var lazy = [];
+      var revive = resolver(input, lazy, new Set(), $);
+      value = revive(value);
+      var i = 0;
+      while (i < lazy.length) {
+        // it could be a lazy.shift() but that's costly
+        var _lazy$i = lazy[i++],
+          o = _lazy$i.o,
+          k = _lazy$i.k,
+          r = _lazy$i.r;
+        o[k] = $.call(o, k, revive(r));
       }
-      return [defs.marker(">"), defs.gutter(gutter), line.length > 0 ? ` ${line}` : "", markerLine].join("");
-    } else {
-      return ` ${defs.gutter(gutter)}${line.length > 0 ? ` ${line}` : ""}`;
     }
-  }).join("\n");
-  if (opts.message && !hasColumns) {
-    frame = `${" ".repeat(numberMaxWidth + 1)}${opts.message}\n${frame}`;
-  }
-  if (shouldHighlight) {
-    return defs.reset(frame);
-  } else {
-    return frame;
-  }
-}
-function index (rawLines, lineNumber, colNumber, opts = {}) {
-  if (!deprecationWarningShown) {
-    deprecationWarningShown = true;
-    const message = "Passing lineNumber and colNumber is deprecated to @babel/code-frame. Please use `codeFrameColumns`.";
-    if (process.emitWarning) {
-      process.emitWarning(message, "DeprecationWarning");
-    } else {
-      const deprecationError = new Error(message);
-      deprecationError.name = "DeprecationWarning";
-      console.warn(new Error(message));
+    return $.call({
+      '': value
+    }, '', value);
+  };
+
+  /**
+   * Converts a JS value into a specialized flatted string.
+   * @param {any} value
+   * @param {((this: any, key: string, value: any) => any) | (string | number)[] | null | undefined} [replacer]
+   * @param {string | number | undefined} [space]
+   * @returns {string}
+   */
+  var stringify = function stringify(value, replacer, space) {
+    var $ = replacer && _typeof(replacer) === object ? function (k, v) {
+      return k === '' || -1 < replacer.indexOf(k) ? v : void 0;
+    } : replacer || noop;
+    var known = new Map();
+    var input = [];
+    var output = [];
+    var i = +set(known, input, $.call({
+      '': value
+    }, '', value));
+    var firstRun = !i;
+    while (i < input.length) {
+      firstRun = true;
+      output[i] = $stringify(input[i++], replace, space);
     }
-  }
-  colNumber = Math.max(colNumber, 0);
-  const location = {
-    start: {
-      column: colNumber,
-      line: lineNumber
+    return '[' + output.join(',') + ']';
+    function replace(key, value) {
+      if (firstRun) {
+        firstRun = !firstRun;
+        return value;
+      }
+      var after = $.call(this, key, value);
+      switch (_typeof(after)) {
+        case object:
+          if (after === null) return after;
+        case primitive:
+          return known.get(after) || set(known, input, after);
+      }
+      return after;
     }
   };
-  return codeFrameColumns(rawLines, location, opts);
-}
 
-exports.codeFrameColumns = codeFrameColumns;
-exports.default = index;
-exports.highlight = highlight;
-//# sourceMappingURL=index.js.map
+  /**
+   * Converts a generic value into a JSON serializable object without losing recursion.
+   * @param {any} value
+   * @returns {any}
+   */
+  var toJSON = function toJSON(value) {
+    return $parse(stringify(value));
+  };
+
+  /**
+   * Converts a previously serialized object with recursion into a recursive one.
+   * @param {any} value
+   * @returns {any}
+   */
+  var fromJSON = function fromJSON(value) {
+    return parse($stringify(value));
+  };
+
+  exports.fromJSON = fromJSON;
+  exports.parse = parse;
+  exports.stringify = stringify;
+  exports.toJSON = toJSON;
+
+  return exports;
+
+})({});
