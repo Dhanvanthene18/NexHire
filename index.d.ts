ab2e93e39da53fd9320f31d318065d1cdf7b22af
S.Dhanvanthene <dhanasaravanan150603@gmail.com>
2026-08-13 22:32:10 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,50 +1,15 @@
-/**
- * Wrap words to a specified length.
- */
-export = wrap;
+type Promisable<T> = T | Promise<T>;
 
-declare function wrap(str: string, options?: wrap.IOptions): string;
-
-declare namespace wrap {
-    export interface IOptions {
-
-        /**
-         * The width of the text before wrapping to a new line.
-         * @default ´50´
-         */
-        width?: number;
-
-        /**
-         * The string to use at the beginning of each line.
-         * @default ´  ´ (two spaces)
-         */
-        indent?: string;
-
-        /**
-         * The string to use at the end of each line.
-         * @default ´\n´
-         */
-        newline?: string;
-
-        /**
-         * An escape function to run on each line after splitting them.
-         * @default (str: string) => string;
-         */
-        escape?: (str: string) => string;
-
-        /**
-         * Trim trailing whitespace from the returned string.
-         * This option is included since .trim() would also strip
-         * the leading indentation from the first line.
-         * @default true
-         */
-        trim?: boolean;
-
-        /**
-         * Break a word between any two letters when the word is longer
-         * than the specified width.
-         * @default false
-         */
-        cut?: boolean;
-    }
+declare namespace escalade {
+	export type Callback = (
+		directory: string,
+		files: string[],
+	) => Promisable<string | false | void>;
 }
+
+declare function escalade(
+	directory: string,
+	callback: escalade.Callback,
+): Promise<string | void>;
+
+export = escalade;
